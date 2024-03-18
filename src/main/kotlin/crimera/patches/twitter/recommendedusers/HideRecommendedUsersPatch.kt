f43f027ef6ab0d5ab0bc3433762e9200a3d8e50d
crimera <stevendavemiranda2@gmail.com>
2024-03-18 08:55:19 +0800

    feat(Twitter): Add Settings for `Hide Recommended Users` patch

--- src/main/kotlin/crimera/patches/twitter/recommendedusers/HideRecommendedUsersPatch.kt
+++ src/main/kotlin/crimera/patches/twitter/recommendedusers/HideRecommendedUsersPatch.kt
@@ -2,16 +2,17 @@ package crimera.patches.twitter.recommendedusers
 
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.getInstructions
-import app.revanced.patcher.extensions.InstructionExtensions.removeInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.stringArrayPatchOption
 import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction 
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import crimera.patches.twitter.misc.settings.SettingsPatch
+import crimera.patches.twitter.misc.settings.fingerprints.SettingsStatusLoadFingerprint
 import crimera.patches.twitter.recommendedusers.fingerprints.HideRecommendedUsersFingerprint
 
 
@@ -34,8 +35,17 @@ object HideRecommendedUsers: BytecodePatch(
         val check = instructions.last { it.opcode == Opcode.IGET_OBJECT }.location.index
         val reg = method.getInstruction<OneRegisterInstruction>(check).registerA
 
-        method.addInstruction(check+1, """
-            const v$reg, 0x0
+        val HIDE_RECOMMENDED_USERS_DESCRIPTOR =
+            "invoke-static {v$reg}, ${SettingsPatch.PREF_DESCRIPTOR};->hideRecommendedUsers(Ljava/util/ArrayList;)Ljava/util/ArrayList;"
+
+        method.addInstructions(check+1, """
+            $HIDE_RECOMMENDED_USERS_DESCRIPTOR
+            move-result-object v$reg
         """.trimIndent())
+
+        SettingsStatusLoadFingerprint.result!!.mutableMethod.addInstruction(
+            0,
+            "${SettingsPatch.SSTS_DESCRIPTOR}->hideRecommendedUsers()V"
+        )
     }
 }
