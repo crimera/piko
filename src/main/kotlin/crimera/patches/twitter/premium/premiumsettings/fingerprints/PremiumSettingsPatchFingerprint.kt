aa947fc2f8a404d0e7509ff95f15b64505b0e576
crimera <stevendavemiranda2@gmail.com>
2024-03-28 01:01:04 +0800

    Revert "feat(Twitter): Add `Premium settings` patch, Unlocks premium options from settings like custom app icon and custom navbar"
    This reverts commit 066e9538

--- src/main/kotlin/crimera/patches/twitter/premium/premiumsettings/fingerprints/PremiumSettingsPatchFingerprint.kt
+++
@@ -1,8 +0,0 @@
-package crimera.patches.twitter.premium.premiumsettings.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-object PremiumSettingsPatchFingerprint: MethodFingerprint(
-    parameters = listOf("Lcom/twitter/navigation/subscriptions/ReferringPage"),
-    strings = listOf("referringPage")
-)
