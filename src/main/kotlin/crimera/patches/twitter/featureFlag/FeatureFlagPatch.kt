2328baf92c733cf99ca364c978514b14b6aae2e0
Swakshan <tsswakshan@gmail.com>
2024-04-09 16:32:58 +0530

    feat(Twitter): Added `Feature Flag Hook`

---
+++ src/main/kotlin/crimera/patches/twitter/featureFlag/FeatureFlagPatch.kt
@@ -0,0 +1,45 @@
+package crimera.patches.twitter.featureFlag
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.getInstructions
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchException
+import crimera.patches.twitter.featureFlag.fingerprints.FeatureFlagFingerprint
+
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
+import com.android.tools.smali.dexlib2.Opcode
+import crimera.patches.twitter.misc.settings.SettingsPatch
+
+@Patch(
+    name = "Hook feature flag",
+    compatiblePackages = [CompatiblePackage("com.twitter.android")],
+    use = true
+)
+@Suppress("unused")
+object FeatureFlagPatch:BytecodePatch(
+    setOf(FeatureFlagFingerprint)
+) {
+    override fun execute(context: BytecodeContext) {
+
+        val result = FeatureFlagFingerprint.result
+            ?:throw PatchException("FeatureFlagFingerprint not found")
+
+        val methods = result.mutableClass.methods
+        val booleanMethod = methods.first { it.returnType == "Z" && it.parameters == listOf("Ljava/lang/String;","Z") }
+
+        val METHOD = """
+            invoke-static {p1,v0}, ${SettingsPatch.PATCHES_DESCRIPTOR}/FeatureSwitchPatch;->flagInfo(Ljava/lang/String;Ljava/lang/Object;)Ljava/lang/Object;
+            move-result-object v0
+        """.trimIndent()
+
+        val loc = booleanMethod.getInstructions().first { it.opcode == Opcode.MOVE_RESULT_OBJECT }.location.index
+
+        booleanMethod.addInstructions(loc+1,METHOD)
+        //end
+    }
+}
