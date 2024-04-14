0fcca927d9230bda9c8db670b4d0f74b7e21a2d1
crimera <stevendavemiranda2@gmail.com>
2024-04-14 14:56:48 +0800

    Add to misc

--- src/main/kotlin/crimera/patches/twitter/featureFlag/FeatureFlagPatch.kt
+++ src/main/kotlin/crimera/patches/twitter/featureFlag/FeatureFlagPatch.kt
@@ -1,6 +1,7 @@
 package crimera.patches.twitter.featureFlag
 
 import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstructions
 import app.revanced.patcher.patch.BytecodePatch
@@ -10,6 +11,7 @@ import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import com.android.tools.smali.dexlib2.Opcode
 import crimera.patches.twitter.misc.settings.SettingsPatch
+import crimera.patches.twitter.misc.settings.fingerprints.SettingsStatusLoadFingerprint
 
 @Patch(
     name = "Hook feature flag",
@@ -37,6 +39,11 @@ object FeatureFlagPatch:BytecodePatch(
         val loc = booleanMethod.getInstructions().first { it.opcode == Opcode.MOVE_RESULT_OBJECT }.location.index
 
         booleanMethod.addInstructions(loc+1,METHOD)
+
+        SettingsStatusLoadFingerprint.result!!.mutableMethod.addInstruction(
+            0,
+            "${SettingsPatch.SSTS_DESCRIPTOR}->enableFeatureFlags()V"
+        )
         //end
     }
 }
