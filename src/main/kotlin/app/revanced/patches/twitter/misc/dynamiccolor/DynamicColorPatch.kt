e3f3491fea403f55d5200d1afc4aebd5c7507c2e
crimera <stevendavemiranda2@gmail.com>
2024-04-17 12:00:34 +0800

    fix: Not creating `values-night-v31` dir

--- src/main/kotlin/app/revanced/patches/twitter/misc/dynamiccolor/DynamicColorPatch.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/dynamiccolor/DynamicColorPatch.kt
@@ -16,33 +16,26 @@ import java.nio.file.Files
 @Suppress("unused")
 object DynamicColorPatch : ResourcePatch() {
     override fun execute(context: ResourceContext) {
-        // directories
         val resDirectory = context["res"]
-        val valuesV31Directory = resDirectory.resolve("values-v31")
-        val valuesNightV31Directory = resDirectory.resolve("values-night-v31")
-        val colorsXml = resDirectory.resolve("colors.xml")
-        val colorsDirectory = "res/values-v31/colors.xml"
-        val colorsNightV31Directory = "res/values-night-v31/colors.xml"
-        val stylesXml = resDirectory.resolve("styles.xml")
-        val stylesNightV31Directory = "res/values-night-v31/styles.xml"
+        if (!resDirectory.isDirectory) throw PatchException("The res folder can not be found.")
 
-        when {
-            !resDirectory.isDirectory -> throw PatchException("The res folder can not be found.")
-            !valuesV31Directory.isDirectory -> Files.createDirectories(valuesV31Directory.toPath())
-            !valuesNightV31Directory.isDirectory -> Files.createDirectories(
-                valuesNightV31Directory.toPath()
-            )
-            !stylesXml.exists() -> FileWriter(stylesXml).write(
-                "<?xml version=\"1.0\" encoding=\"utf-8\"?><resources></resources>"
-            )
-            !colorsXml.exists() -> listOf(valuesV31Directory, valuesNightV31Directory).forEach { _ ->
-                FileWriter(colorsXml).write(
-                    "<?xml version=\"1.0\" encoding=\"utf-8\"?><resources></resources>"
-                )
+        val valuesV31Directory = resDirectory.resolve("values-v31")
+        if (!valuesV31Directory.isDirectory) Files.createDirectories(valuesV31Directory.toPath())
+
+        val valuesNightV31Directory = resDirectory.resolve("values-night-v31")
+        if (!valuesNightV31Directory.isDirectory) Files.createDirectories(valuesNightV31Directory.toPath())
+
+        listOf(valuesV31Directory, valuesNightV31Directory).forEach { it ->
+            val colorsXml = it.resolve("colors.xml")
+
+            if (!colorsXml.exists()) {
+                FileWriter(colorsXml).use {
+                    it.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><resources></resources>")
+                }
             }
         }
 
-        context.xmlEditor[colorsDirectory].use { editor ->
+        context.xmlEditor["res/values-v31/colors.xml"].use { editor ->
             val document = editor.file
 
             mapOf(
@@ -64,7 +57,7 @@ object DynamicColorPatch : ResourcePatch() {
             }
         }
 
-        context.xmlEditor[colorsNightV31Directory].use { editor ->
+        context.xmlEditor["res/values-night-v31/colors.xml"].use { editor ->
             val document = editor.file
 
             mapOf(
@@ -87,7 +80,7 @@ object DynamicColorPatch : ResourcePatch() {
         /** fun fullMaterialDesign() { **/
         // backward compatible, creates style into v31 res dir (A12+)
         // replace parts of DimTheme with user's material 3 neutral palette
-        context.xmlEditor[stylesNightV31Directory].use { editor ->
+        context.xmlEditor["res/values-night-v31/colors.xml"].use { editor ->
             val document = editor.file
 
             val newStyle = document.createElement("style")
