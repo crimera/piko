f89f50d69170b76d1dd663583da4af766169b1b9
crimera <stevendavemiranda2@gmail.com>
2024-05-18 09:21:04 +0800

    implemented basic image downloading

--- src/main/kotlin/crimera/patches/all/appDowngrading/AppDowngradingPatch.kt
+++ src/main/kotlin/crimera/patches/all/appDowngrading/AppDowngradingPatch.kt
@@ -5,13 +5,13 @@ import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.Patch
 import org.w3c.dom.Element
 
-
 @Patch(
-    name = "Enable app downgrading",
-    description = "Sets app version to a default value making installation of different versions possible",
-    use = false
+        name = "Enable app downgrading",
+        description =
+                "Sets app version to a default value making installation of different versions possible",
+        use = false
 )
-object AppDowngradingPatch: ResourcePatch() {
+object AppDowngradingPatch : ResourcePatch() {
     override fun execute(context: ResourceContext) {
         context.xmlEditor["AndroidManifest.xml"].use {
             val manifestElement = it.file.getElementsByTagName("manifest").item(0) as Element
--- src/main/kotlin/crimera/patches/instagram/interaction/download/DownloadPatch.kt
+++ src/main/kotlin/crimera/patches/instagram/interaction/download/DownloadPatch.kt
@@ -19,59 +19,74 @@ import crimera.patches.instagram.interaction.download.fingerprints.itemclickedcl
 import crimera.patches.instagram.interaction.download.fingerprints.itemclickedclasses.PostMediaFingerprint
 import crimera.patches.instagram.misc.integrations.IntegrationsPatch
 
-
 @Patch(
-    name = "Download patch",
-    compatiblePackages = [CompatiblePackage("com.instagram.android")],
-    requiresIntegrations = true,
-    dependencies = [SetupHookSignaturesPatch::class, IntegrationsPatch::class]
+        name = "Download patch",
+        compatiblePackages = [CompatiblePackage("com.instagram.android")],
+        requiresIntegrations = true,
+        dependencies = [SetupHookSignaturesPatch::class, IntegrationsPatch::class]
 )
-object DownloadPatch : BytecodePatch(
-    setOf(
-        DialogItemClickedFingerprint,
-        FeedBottomSheetFingerprint,
-
-        PostMediaFingerprint,
-        MediaViewFingerprint,
-        DeviceSessionClassFingerprint
-    )
-) {
+object DownloadPatch :
+        BytecodePatch(
+                setOf(
+                        DialogItemClickedFingerprint,
+                        FeedBottomSheetFingerprint,
+                        PostMediaFingerprint,
+                        MediaViewFingerprint,
+                        DeviceSessionClassFingerprint
+                )
+        ) {
     override fun execute(context: BytecodeContext) {
         FeedBottomSheetFingerprint.result?.mutableMethod?.let { method ->
-            val loc = method.getInstructions().filter { it.opcode == Opcode.GOTO }[2].location.index + 3
+            val loc =
+                    method.getInstructions().filter { it.opcode == Opcode.GOTO }[2].location.index +
+                            3
             method.addInstructions(
-                loc,
-//                TODO make the register dynamic
-                """
+                    loc,
+                    // TODO make the register dynamic
+                    """
                   invoke-static {v5}, Lapp/revanced/integrations/instagram/patches/DownloadPatch;->addDownloadButton(Ljava/util/List;)V
                 """
             )
-        } ?: throw FeedBottomSheetFingerprint.exception
+        }
+                ?: throw FeedBottomSheetFingerprint.exception
 
         DialogItemClickedFingerprint.result?.mutableMethod?.let { method ->
-            val loc = method.getInstructions().first { it.opcode == Opcode.PACKED_SWITCH }.location.index
+            val loc =
+                    method.getInstructions()
+                            .first { it.opcode == Opcode.PACKED_SWITCH }
+                            .location
+                            .index
 
             method.replaceInstruction(
-//                TODO should make registers dynamic
-                method.getInstructions().filter { it.opcode == Opcode.CONST_4 }[1].location.index,
-                "const/4 v0, 0x0"
+                    // TODO should make registers dynamic
+                    method.getInstructions().filter { it.opcode == Opcode.CONST_4 }[1]
+                            .location
+                            .index,
+                    "const/4 v0, 0x0"
             )
 
             val itemClickedClass = method.definingClass
             val postMediaClass =
-                PostMediaFingerprint.result?.method?.definingClass ?: throw PostMediaFingerprint.exception
+                    PostMediaFingerprint.result?.method?.definingClass
+                            ?: throw PostMediaFingerprint.exception
 
-            val mediaViewResult = MediaViewFingerprint.result?.method ?: throw MediaViewFingerprint.exception
+            val mediaViewResult =
+                    MediaViewFingerprint.result?.method ?: throw MediaViewFingerprint.exception
             val getMediaListSizeMethod =
-                MediaViewFingerprint.result?.classDef?.methods?.first { it.returnType == "I" && it.implementation?.registerCount == 2 }?.name
-                    ?: throw MediaViewFingerprint.exception
+                    MediaViewFingerprint.result?.classDef?.methods
+                            ?.first {
+                                it.returnType == "I" && it.implementation?.registerCount == 2
+                            }
+                            ?.name
+                            ?: throw MediaViewFingerprint.exception
 
-            val deviceSessionClass = DeviceSessionClassFingerprint.result?.method?.definingClass
-                ?: throw DeviceSessionClassFingerprint.exception
+            val deviceSessionClass =
+                    DeviceSessionClassFingerprint.result?.method?.definingClass
+                            ?: throw DeviceSessionClassFingerprint.exception
 
             method.addInstructionsWithLabels(
-                loc,
-                """
+                    loc,
+                    """
                     const/16 v4, 0x60
                     if-ne v2, v4, :cond_0
                     
@@ -82,19 +97,20 @@ object DownloadPatch : BytecodePatch(
                     move-result v4
                     
                     iget-object v6, v0, $itemClickedClass->A09:$deviceSessionClass
-                    iget-object v5, v0, $itemClickedClass->A04:Landroidx/fragment/app/FragmentActivity;                    
+                    iget-object v5, v0, $itemClickedClass->A04:Landroidx/fragment/app/FragmentActivity;
                     
                     invoke-static {v2, v4, v6, v5}, Lapp/revanced/integrations/instagram/patches/DownloadPatch;->downloadPost(Ljava/lang/Object;ILjava/lang/Object;Landroid/app/Activity;)V
                     
                     const-string v0, "BRUH"
                     const-string v1, "Download clicked"
-                    invoke-static {v0, v1}, Landroid/util/Log;->d(Ljava/lang/String;Ljava/lang/String;)I                    
+                    invoke-static {v0, v1}, Landroid/util/Log;->d(Ljava/lang/String;Ljava/lang/String;)I
 
                     return-void 
                 """,
-                ExternalLabel("cond_0", method.getInstruction(loc))
+                    ExternalLabel("cond_0", method.getInstruction(loc))
             )
-
-        } ?: throw DialogItemClickedFingerprint.exception
+        }
+                ?: throw DialogItemClickedFingerprint.exception
     }
 }
+
