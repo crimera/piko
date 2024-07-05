3d5941e226c1fb9402edeeb138c1c974026e0598
Swakshan <tsswakshan@gmail.com>
2024-07-06 00:02:46 +0530

    feat(Twitter): Added `Custom downloader`

---
+++ src/main/kotlin/crimera/patches/twitter/misc/nativeDownloader/fingerprints/NativeDownloaderAlwaysIcon.kt
@@ -0,0 +1,9 @@
+package crimera.patches.twitter.misc.nativeDownloader.fingerprints
+
+import app.revanced.patcher.fingerprint.MethodFingerprint
+
+object NativeDownloaderAlwaysIcon:MethodFingerprint(
+    strings = listOf(
+        "View in Tweet Sandbox",
+    )
+)
