a27675a2b6324623e3ec09236fe6be2a51b2a22a
swakwork <swakwork+git@gmail.com>
2026-01-05 00:21:20 +0530

    update(Twitter): `Custom sharing domain` : added support for new share sheet

--- patches/src/main/kotlin/app/crimera/patches/twitter/link/customsharingdomain/CustomSharingDomainPatch.kt
+++ patches/src/main/kotlin/app/crimera/patches/twitter/link/customsharingdomain/CustomSharingDomainPatch.kt
@@ -4,10 +4,20 @@ import app.crimera.patches.twitter.link.cleartrackingparams.addSessionTokenFinge
 import app.crimera.patches.twitter.misc.settings.settingsPatch
 import app.crimera.patches.twitter.misc.settings.settingsStatusLoadFingerprint
 import app.crimera.utils.Constants.PATCHES_DESCRIPTOR
-import app.crimera.utils.Constants.PREF_DESCRIPTOR
 import app.crimera.utils.enableSettings
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.fingerprint
 import app.revanced.patcher.patch.bytecodePatch
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+
+internal const val TARGET_STRING = "https://x.com/i/status/"
+internal val newShareSheetLinkFingerprint =
+    fingerprint {
+        strings(
+            TARGET_STRING,
+        )
+    }
 
 @Suppress("unused")
 val customSharingDomainPatch =
@@ -18,13 +28,31 @@ val customSharingDomainPatch =
         compatibleWith("com.twitter.android")
         dependsOn(settingsPatch)
         execute {
+            val methodInvoke = "$PATCHES_DESCRIPTOR/links/Urls;->changeDomain(Ljava/lang/String;)Ljava/lang/String;"
             addSessionTokenFingerprint.method.addInstructions(
                 0,
                 """
-                invoke-static {p0},  $PATCHES_DESCRIPTOR/links/Urls;->changeDomain(Ljava/lang/String;)Ljava/lang/String;
+                invoke-static {p0}, $methodInvoke
                 move-result-object p0
                 """.trimIndent(),
             )
             settingsStatusLoadFingerprint.enableSettings("enableCustomSharingDomain")
+
+            try {
+                // Should be applied only post 11.48.xx in new share sheet.
+                newShareSheetLinkFingerprint.method.apply {
+                    val strIndx = newShareSheetLinkFingerprint.stringMatches!!.first { it.string == TARGET_STRING }.index
+                    val reg = getInstruction<OneRegisterInstruction>(strIndx).registerA
+
+                    addInstructions(
+                        strIndx + 1,
+                        """
+                        invoke-static {v$reg}, $methodInvoke
+                        move-result-object v$reg
+                        """.trimIndent(),
+                    )
+                }
+            } catch (_: Exception) {
+            }
         }
     }
