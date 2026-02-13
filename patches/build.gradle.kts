91e563cda0ddd7a5d05a9ce3b037a60dac03e9a2
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-02-13 13:21:30 +0100

    ci: Fix build

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -17,6 +17,21 @@ dependencies {
     implementation(libs.gson)
 }
 
+tasks {
+    register<JavaExec>("generatePatchesList") {
+        description = "Build patch with patch list"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.morphe.util.PatchListGeneratorKt")
+    }
+    // Used by gradle-semantic-release-plugin.
+    publish {
+        dependsOn("generatePatchesList")
+    }
+}
+
 kotlin {
     compilerOptions {
         freeCompilerArgs = listOf("-Xcontext-receivers")
