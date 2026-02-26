27e79fba047825363cbc42c7f91557aec973c5c4
swakwork <swakwork+git@gmail.com>
2026-02-26 21:59:40 +0530

    feat(Instagram): Added `Distraction free` patches

---
+++ patches/src/main/kotlin/app/crimera/patches/instagram/links/distractionFree/DisableFeedPatch.kt
@@ -0,0 +1,19 @@
+package app.crimera.patches.instagram.links.distractionFree
+
+import app.crimera.patches.instagram.links.interceptUriPatch
+import app.crimera.patches.instagram.misc.settings.settingsPatch
+import app.crimera.patches.instagram.utils.enableSettings
+import app.morphe.patcher.patch.bytecodePatch
+
+
+@Suppress("unused")
+val disableFeedPatch = bytecodePatch(
+    name = "Disable feed",
+) {
+    dependsOn(settingsPatch, interceptUriPatch)
+    compatibleWith("com.instagram.android")
+
+    execute {
+        enableSettings("disableFeed")
+    }
+}
