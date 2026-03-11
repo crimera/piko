9358f66cc2d38013a5a98b6aefd0f5e1c932017e
crimera <44558091+crimera@users.noreply.github.com>
2026-03-11 17:17:23 +0800

    chore: add headers

--- extensions/twitter/src/main/java/app/morphe/extension/twitter/settings/fragments/SettingsAboutFragment.java
+++ extensions/twitter/src/main/java/app/morphe/extension/twitter/settings/fragments/SettingsAboutFragment.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2026 piko <https://github.com/crimera/piko>
+ *
+ * This file is part of piko.
+ *
+ * Any modifications, derivatives, or substantial rewrites of this file
+ * must retain this copyright notice and the piko attribution 
+ * in the source code and version control history.
+ */
+
 package app.morphe.extension.twitter.settings.fragments;
 
 import android.text.*;
--- patches/src/main/kotlin/app/crimera/patches/twitter/misc/customize/font/CustomEmojiFont.kt
+++ patches/src/main/kotlin/app/crimera/patches/twitter/misc/customize/font/CustomEmojiFont.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2026 piko <https://github.com/crimera/piko>
+ *
+ * This file is part of piko.
+ *
+ * Any modifications, derivatives, or substantial rewrites of this file
+ * must retain this copyright notice and the piko attribution 
+ * in the source code and version control history.
+ */
+
 package app.crimera.patches.twitter.misc.customize.font
 
 import app.crimera.patches.twitter.misc.settings.settingsPatch
