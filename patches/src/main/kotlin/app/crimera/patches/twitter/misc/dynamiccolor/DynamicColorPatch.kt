fcd1aa33fecb0eaaa76b0168d78a56c074874e36
crimera <44558091+crimera@users.noreply.github.com>
2026-03-15 17:02:04 +0800

    fix(Twitter - Dynamic Color): update border and icon tint colors

--- patches/src/main/kotlin/app/crimera/patches/twitter/misc/dynamiccolor/DynamicColorPatch.kt
+++ patches/src/main/kotlin/app/crimera/patches/twitter/misc/dynamiccolor/DynamicColorPatch.kt
@@ -40,12 +40,12 @@ val dynamicColorPatch = resourcePatch(
             mapOf(
                 "ps__twitter_blue" to "@color/twitter_blue",
                 "ps__twitter_blue_pressed" to "@color/twitter_blue_fill_pressed",
-                "twitter_blue" to "@android:color/system_accent1_500",
-                "twitter_blue_fill_pressed" to "@android:color/system_accent1_300",
-                "twitter_blue_opacity_30" to "@android:color/system_accent1_100",
-                "twitter_blue_opacity_50" to "@android:color/system_accent1_200",
-                "twitter_blue_opacity_58" to "@android:color/system_accent1_300",
-                "deep_transparent_twitter_blue" to "@android:color/system_accent1_200",
+                "twitter_blue" to "@color/m3_sys_color_dynamic_light_primary",
+                "twitter_blue_fill_pressed" to "@color/m3_sys_color_dynamic_light_primary_container",
+                "twitter_blue_opacity_30" to "@color/material_dynamic_primary95",
+                "twitter_blue_opacity_50" to "@color/material_dynamic_primary90",
+                "twitter_blue_opacity_58" to "@color/material_dynamic_primary80",
+                "deep_transparent_twitter_blue" to "@color/material_dynamic_primary90",
             ).forEach { (k, v) ->
                 val colorElement = document.createElement("color")
 
@@ -59,12 +59,12 @@ val dynamicColorPatch = resourcePatch(
         document("res/values-night-v31/colors.xml").use { document ->
             val resourcesElement = document.documentElement
             mapOf(
-                "twitter_blue" to "@android:color/system_accent1_200",
-                "twitter_blue_fill_pressed" to "@android:color/system_accent1_300",
-                "twitter_blue_opacity_30" to "@android:color/system_accent1_50",
-                "twitter_blue_opacity_50" to "@android:color/system_accent1_100",
-                "twitter_blue_opacity_58" to "@android:color/system_accent1_200",
-                "deep_transparent_twitter_blue" to "@android:color/system_accent1_200",
+                "twitter_blue" to "@color/m3_sys_color_dynamic_dark_primary",
+                "twitter_blue_fill_pressed" to "@color/m3_sys_color_dynamic_dark_primary_container",
+                "twitter_blue_opacity_30" to "@color/material_dynamic_primary30",
+                "twitter_blue_opacity_50" to "@color/material_dynamic_primary40",
+                "twitter_blue_opacity_58" to "@color/material_dynamic_primary50",
+                "deep_transparent_twitter_blue" to "@color/m3_sys_color_dynamic_dark_primary_container",
             ).forEach { (k, v) ->
                 val colorElement = document.createElement("color")
 
@@ -75,38 +75,69 @@ val dynamicColorPatch = resourcePatch(
             }
         }
 
-        // replace parts of DimTheme with user's material 3 neutral palette
+        // replace dark palettes with user's material 3 colors
         document("res/values-night-v31/styles.xml").use { document ->
-            val newStyle = document.createElement("style")
-            newStyle.setAttribute("name", "PaletteDim")
-            newStyle.setAttribute("parent", "@style/HorizonColorPaletteDark")
+            val dimStyle = document.createElement("style")
+            dimStyle.setAttribute("name", "PaletteDim")
+            dimStyle.setAttribute("parent", "@style/HorizonColorPaletteDark")
 
-            val styleItems = mapOf(
-                "abstractColorCellBackground" to "@color/material_dynamic_neutral10",
-                "abstractColorCellBackgroundTranslucent" to "@color/material_dynamic_neutral10",
-                "abstractColorDeepGray" to "#ff8899a6",
-                "abstractColorDivider" to "#ff38444d",
-                "abstractColorFadedGray" to "@color/material_dynamic_neutral10",
-                "abstractColorFaintGray" to "@color/material_dynamic_neutral10",
-                "abstractColorHighlightBackground" to "@color/material_dynamic_neutral20",
-                "abstractColorLightGray" to "#ff3d5466",
+            val dimStyleItems = mapOf(
+                "abstractColorCellBackground" to "@color/m3_sys_color_dynamic_dark_background",
+                "abstractColorCellBackgroundTranslucent" to "@color/m3_sys_color_dynamic_dark_surface_container_low",
+                "abstractColorDeepGray" to "@color/m3_sys_color_dynamic_dark_on_surface_variant",
+                "abstractColorDivider" to "@color/m3_sys_color_dynamic_dark_outline_variant",
+                "abstractColorFadedGray" to "@color/m3_sys_color_dynamic_dark_surface_container_lowest",
+                "abstractColorFaintGray" to "@color/m3_sys_color_dynamic_dark_surface_container_low",
+                "abstractColorHighlightBackground" to "@color/m3_sys_color_dynamic_dark_surface_container",
+                "abstractColorLightGray" to "@color/m3_sys_color_dynamic_dark_outline_variant",
                 "abstractColorLink" to "@color/twitter_blue",
-                "abstractColorMediumGray" to "#ff6b7d8c",
-                "abstractColorText" to "@color/white",
-                "abstractColorUnread" to "#ff163043",
-                "abstractElevatedBackground" to "#ff1c2c3c",
-                "abstractElevatedBackgroundShadow" to "#1a15202b"
+                "abstractColorMediumGray" to "@color/m3_sys_color_dynamic_dark_outline",
+                "abstractColorText" to "@color/m3_sys_color_dynamic_dark_on_surface",
+                "abstractColorUnread" to "@color/m3_sys_color_dynamic_dark_primary_container",
+                "abstractElevatedBackground" to "@color/m3_sys_color_dynamic_dark_surface_container_high",
+                "abstractElevatedBackgroundShadow" to "@color/black_opacity_10"
             )
 
-            styleItems.forEach { (k, v) ->
+            dimStyleItems.forEach { (k, v) ->
                 val styleElement = document.createElement("item")
 
                 styleElement.setAttribute("name", k)
                 styleElement.textContent = v
-                newStyle.appendChild(styleElement)
+                dimStyle.appendChild(styleElement)
             }
 
-            document.documentElement.appendChild(newStyle)
+            document.documentElement.appendChild(dimStyle)
+
+            val lightsOutStyle = document.createElement("style")
+            lightsOutStyle.setAttribute("name", "PaletteLightsOut")
+            lightsOutStyle.setAttribute("parent", "@style/HorizonColorPaletteDark")
+
+            val lightsOutStyleItems = mapOf(
+                "abstractColorCellBackground" to "@color/black",
+                "abstractColorCellBackgroundTranslucent" to "@color/black_opacity_50",
+                "abstractColorDeepGray" to "@color/m3_sys_color_dynamic_dark_on_surface_variant",
+                "abstractColorDivider" to "@color/m3_sys_color_dynamic_dark_outline_variant",
+                "abstractColorFadedGray" to "@color/m3_sys_color_dynamic_dark_surface_container_low",
+                "abstractColorFaintGray" to "@color/m3_sys_color_dynamic_dark_surface_container_lowest",
+                "abstractColorHighlightBackground" to "@color/m3_sys_color_dynamic_dark_surface_container_low",
+                "abstractColorLightGray" to "@color/m3_sys_color_dynamic_dark_outline_variant",
+                "abstractColorLink" to "@color/twitter_blue",
+                "abstractColorMediumGray" to "@color/m3_sys_color_dynamic_dark_outline",
+                "abstractColorText" to "@color/m3_sys_color_dynamic_dark_on_surface",
+                "abstractColorUnread" to "@color/m3_sys_color_dynamic_dark_primary_container",
+                "abstractElevatedBackground" to "@color/m3_sys_color_dynamic_dark_surface_container",
+                "abstractElevatedBackgroundShadow" to "@color/black_opacity_10"
+            )
+
+            lightsOutStyleItems.forEach { (k, v) ->
+                val styleElement = document.createElement("item")
+
+                styleElement.setAttribute("name", k)
+                styleElement.textContent = v
+                lightsOutStyle.appendChild(styleElement)
+            }
+
+            document.documentElement.appendChild(lightsOutStyle)
         }
 
         // monet theme in light mode
@@ -116,19 +147,19 @@ val dynamicColorPatch = resourcePatch(
             newStyle.setAttribute("parent", "@style/HorizonColorPaletteLight")
 
             val styleItems = mapOf(
-                "abstractColorCellBackground" to "@android:color/system_neutral2_0",
-                "abstractColorCellBackgroundTranslucent" to "@android:color/system_neutral2_0",
-                "abstractColorDeepGray" to "@color/gray_1000",
-                "abstractColorDivider" to "@android:color/system_accent1_400",
-                "abstractColorFadedGray" to "@color/material_dynamic_primary99",
-                "abstractColorFaintGray" to "@color/material_dynamic_primary99",
-                "abstractColorHighlightBackground" to "@android:color/system_accent2_100",
-                "abstractColorLightGray" to "@color/gray_1100",
+                "abstractColorCellBackground" to "@color/m3_sys_color_dynamic_light_surface",
+                "abstractColorCellBackgroundTranslucent" to "@color/m3_sys_color_dynamic_light_surface_container_low",
+                "abstractColorDeepGray" to "@color/m3_sys_color_dynamic_light_on_surface_variant",
+                "abstractColorDivider" to "@color/m3_sys_color_dynamic_light_outline_variant",
+                "abstractColorFadedGray" to "@color/m3_sys_color_dynamic_light_surface_container",
+                "abstractColorFaintGray" to "@color/m3_sys_color_dynamic_light_surface_container_low",
+                "abstractColorHighlightBackground" to "@color/m3_sys_color_dynamic_light_surface_container_high",
+                "abstractColorLightGray" to "@color/m3_sys_color_dynamic_light_outline_variant",
                 "abstractColorLink" to "@color/twitter_blue",
-                "abstractColorMediumGray" to "@color/gray_1100",
-                "abstractColorText" to "#0d1115",
-                "abstractColorUnread" to "@color/blue_0",
-                "abstractElevatedBackground" to "@color/white",
+                "abstractColorMediumGray" to "@color/m3_sys_color_dynamic_light_outline",
+                "abstractColorText" to "@color/m3_sys_color_dynamic_light_on_surface",
+                "abstractColorUnread" to "@color/m3_sys_color_dynamic_light_primary_container",
+                "abstractElevatedBackground" to "@color/m3_sys_color_dynamic_light_surface_container_low",
                 "abstractElevatedBackgroundShadow" to "@color/black_opacity_10"
             )
 
