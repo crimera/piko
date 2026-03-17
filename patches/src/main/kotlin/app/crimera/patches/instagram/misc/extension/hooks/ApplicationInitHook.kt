5195f696c2b0fd4a8b0cbe3c6513be8ffae78b05
swakwork <swakwork+git@gmail.com>
2026-03-17 11:45:00 +0530

    update(Instagram): Extension hook context extraction

--- patches/src/main/kotlin/app/crimera/patches/instagram/misc/extension/hooks/ApplicationInitHook.kt
+++ patches/src/main/kotlin/app/crimera/patches/instagram/misc/extension/hooks/ApplicationInitHook.kt
@@ -1,7 +1,10 @@
 package app.crimera.patches.instagram.misc.extension.hooks
 
 import app.morphe.patcher.Fingerprint
+import app.morphe.patcher.extensions.InstructionExtensions.instructions
 import app.morphe.shared.misc.extension.extensionHook
+import app.morphe.util.registersUsed
+import com.android.tools.smali.dexlib2.Opcode
 
 internal val applicationInitHook =
     extensionHook(
@@ -12,4 +15,17 @@ internal val applicationInitHook =
                     classDef.endsWith("/InstagramAppShell;")
                 },
             ),
+        insertIndexResolver = { method ->
+            method.instructions.indexOfFirst { instruction ->
+                instruction.opcode == Opcode.INVOKE_SUPER
+            } + 1
+        },
+        contextRegisterResolver = { method ->
+            val invokeSuperInstruction =
+                method.instructions.first { instruction ->
+                    instruction.opcode == Opcode.INVOKE_SUPER
+                }
+
+            "v${invokeSuperInstruction.registersUsed[0]}"
+        },
     )
