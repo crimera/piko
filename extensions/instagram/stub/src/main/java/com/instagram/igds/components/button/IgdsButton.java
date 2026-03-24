eac2f8ad3425e0d7b523e56a87c40da63c5114d3
swakwork <swakwork+git@gmail.com>
2026-03-24 12:31:53 +0530

    refactor(Instagram): Refactored settings button

---
+++ extensions/instagram/stub/src/main/java/com/instagram/igds/components/button/IgdsButton.java
@@ -0,0 +1,10 @@
+package com.instagram.igds.components.button;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+public class IgdsButton extends FrameLayout{
+    public IgdsButton(Context context){super(context);};
+
+    public void setText(String str){}
+}
