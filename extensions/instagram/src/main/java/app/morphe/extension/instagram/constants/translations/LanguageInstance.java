caf8ffb7704ae514a3b5f98ae265878214d42ae5
swakwork <swakwork+git@gmail.com>
2026-05-03 21:05:59 +0530

    feat(Instagram): Initial support for translations

---
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/constants/translations/LanguageInstance.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2026 piko <https://github.com/crimera/piko>
+ *
+ * This file is part of piko.
+ *
+ * Any modifications, derivatives, or substantial rewrites of this file
+ * must retain this copyright notice and the piko attribution
+ * in the source code and version control history.
+ */
+
+
+package app.morphe.extension.instagram.constants.translations;
+
+import java.util.Locale;
+
+public class LanguageInstance {
+
+    public static DefaultStrings getLanguageInstance() {
+        String lang = Locale.getDefault().getLanguage();
+
+        if (lang.equals("hi")) {
+            return new StringsHindi();
+        }
+
+        return new StringsEnglish(); // Default fallback
+    }
+}
---
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/constants/translations/StringsHindi.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2026 piko <https://github.com/crimera/piko>
+ *
+ * This file is part of piko.
+ *
+ * Any modifications, derivatives, or substantial rewrites of this file
+ * must retain this copyright notice and the piko attribution
+ * in the source code and version control history.
+ */
+
+
+package app.morphe.extension.instagram.constants.translations;
+
+public class StringsHindi extends DefaultStrings {
+    public StringsHindi() {
+        this.PIKO_SETTINGS_TITLE = "Piko सेटिंग्स";
+        this.CATEGORY_ADS = "विज्ञापन";
+        this.DISABLE_ADS = "विज्ञापन बंद करें";
+        this.HIDE_SUGEESTED_CONTENT = "सुझाई गई सामग्री छिपाएँ";
+        this.HIDE_SUGEESTED_CONTENT_DESC = "सुझाई गई कहानियों, रील्स और थ्रेड्स को छिपाता है (सुझाई गई पोस्ट्स फिर भी दिखाई देंगी)";
+
+        this.MORE_PROFILE_OPTIONS = "प्रोफ़ाइल के और विकल्प";
+
+    }
+}
