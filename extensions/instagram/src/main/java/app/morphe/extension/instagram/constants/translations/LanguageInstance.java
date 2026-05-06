40e16445e6652cf457bce470c1ddfe2c53d0c42e
krvstek <138402769+krvstek@users.noreply.github.com>
2026-05-06 22:11:35 +0200

    feat(Instagram): Add `Polish` translation

--- extensions/instagram/src/main/java/app/morphe/extension/instagram/constants/translations/LanguageInstance.java
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/constants/translations/LanguageInstance.java
@@ -22,6 +22,8 @@ public class LanguageInstance {
             return new StringsHindi();
         } else if (lang.equals("ko")) {
             return new StringsKorean();
+        } else if (lang.equals("pl")) {
+            return new StringsPolish();
         }
 
         return new StringsEnglish(); // Default fallback
