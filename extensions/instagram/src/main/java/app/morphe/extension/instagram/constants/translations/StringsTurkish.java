b2e34e4c3a833d3b847dce338107a881864a8557
Runterya <97337724+Runteryaa@users.noreply.github.com>
2026-05-10 23:28:24 +0300

    Update StringsTurkish.java

--- extensions/instagram/src/main/java/app/morphe/extension/instagram/constants/translations/StringsTurkish.java
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/constants/translations/StringsTurkish.java
@@ -10,7 +10,8 @@
 
 package app.morphe.extension.instagram.constants.translations;
 
-public class StringsTurkish {
+public class StringsTurkish extends DefaultStrings{
+  public class StringsTurkish()
     public static String PIKO_SETTINGS_TITLE = "Piko ayarları";
 
     public static String CATEGORY_ADS = "Reklamlar";
