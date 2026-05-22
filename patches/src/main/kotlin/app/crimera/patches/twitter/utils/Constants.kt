b9e221ded19164779b6a3e036074b8adb4b3f9b7
swakwork <swakwork+git@gmail.com>
2026-05-22 22:29:18 +0530

    chore(Twitter): Temporarily remove APK signature check

--- patches/src/main/kotlin/app/crimera/patches/twitter/utils/Constants.kt
+++ patches/src/main/kotlin/app/crimera/patches/twitter/utils/Constants.kt
@@ -11,13 +11,6 @@ object Constants {
             packageName = "com.twitter.android",
             apkFileType = ApkFileType.APKM,
             appIconColor = 0x000000,
-            signatures =
-                setOf(
-                    // APK
-                    "0fd9a0cfb07b65950997b4eaebdc53931392391aa406538a3b04073bc2ce2fe9",
-                    // APKM
-                    "45c53db089fb8d63a1c71154037c414b1b0646a141a73ae6e3bcbf8cd148402f",
-                ),
             targets =
                 listOf(
                     // Beta
