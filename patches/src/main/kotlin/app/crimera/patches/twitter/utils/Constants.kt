dd7352cd9485a7af6d32a62b09410d65fd4567d2
swakwork <swakwork+git@gmail.com>
2026-05-22 22:29:46 +0530

    bump(Twitter): Added support for 11.93.xx

--- patches/src/main/kotlin/app/crimera/patches/twitter/utils/Constants.kt
+++ patches/src/main/kotlin/app/crimera/patches/twitter/utils/Constants.kt
@@ -30,7 +30,7 @@ object Constants {
                     ),
                     // PairIP version
                     AppTarget(
-                        version = "11.91.0-release-ripped.0",
+                        version = "11.93.0-release-ripped.0",
                         description = "Make sure the APK is PairIP bypassed (Check the support group)",
                     ),
                 ),
