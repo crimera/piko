f4d2e53171621d02f2b04c0a0d4ff220e2e4c799
swakwork <swakwork+git@gmail.com>
2026-06-28 16:53:52 +0530

    fix(Twitter): Fix `Custom share menu` crash on image view

--- patches/src/main/kotlin/app/crimera/patches/twitter/misc/shareMenu/hooks/ShareMenuButtonOnClickHook.kt
+++ patches/src/main/kotlin/app/crimera/patches/twitter/misc/shareMenu/hooks/ShareMenuButtonOnClickHook.kt
@@ -6,9 +6,7 @@
 
 package app.crimera.patches.twitter.misc.shareMenu.hooks
 
-import app.crimera.patches.twitter.entity.decoder.TIMELINE_ITEM_CLASS_NAME
 import app.crimera.patches.twitter.entity.decoder.TWEET_ITEM_CLASS_NAME
-import app.crimera.patches.twitter.entity.decoder.TimelineItemDebugDialogInfoBuilderFingerprint
 import app.crimera.patches.twitter.entity.decoder.decoderEntity
 import app.crimera.patches.twitter.misc.shareMenu.fingerprints.ShareMenuButtonFuncCallFingerprint
 import app.crimera.patches.twitter.utils.Constants.COMPATIBILITY_X
@@ -19,6 +17,7 @@ import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.util.smali.ExternalLabel
 import app.morphe.util.indexOfFirstInstruction
 import app.morphe.util.registersUsed
+import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
 internal const val EXTENSION_CLASS_NAME = "${NATIVE_DESCRIPTOR}/ButtonPressHandler;->"
@@ -33,41 +32,40 @@ val shareMenuButtonOnClickHook =
 
         execute {
 
-            ShareMenuButtonFuncCallFingerprint.method.apply {
-                val contextIfEqzIndex = indexOfFirstInstruction(Opcode.IF_EQZ)
-                val contextRegister = getInstruction(contextIfEqzIndex).registersUsed[0]
+            ShareMenuButtonFuncCallFingerprint.apply {
 
-                val buttonEnumPressCheckIndex = indexOfFirstInstruction(contextIfEqzIndex, Opcode.IF_NE)
-                val buttonPressedRegister = getInstruction(buttonEnumPressCheckIndex).registersUsed[0]
+                val isStaticMethod = AccessFlags.STATIC.isSet(method.accessFlags)
 
-                val timelineItemRegister =
-                    getInstruction(
-                        indexOfFirstInstruction(buttonEnumPressCheckIndex, Opcode.INSTANCE_OF),
-                    ).registersUsed[1]
-
-                val tweetFieldFromTimelineItem =
-                    TimelineItemDebugDialogInfoBuilderFingerprint.classDef.fields.first {
-                        it.type ==
-                            TWEET_ITEM_CLASS_NAME
+                method.apply {
+                    var tweetObjectParameter = parameters.indexOfFirst { it.type == TWEET_ITEM_CLASS_NAME }
+                    // If it is not a static function, then we need to increase the parameter count by one.
+                    if (!isStaticMethod) {
+                        tweetObjectParameter += 1
                     }
 
-                val nextInstructionIndex = contextIfEqzIndex + 1
-                val nextInstruction = getInstruction(nextInstructionIndex)
-                val freeRegister = nextInstruction.registersUsed[0]
+                    val contextIfEqzIndex = indexOfFirstInstruction(Opcode.IF_EQZ)
+                    val contextRegister = getInstruction(contextIfEqzIndex).registersUsed[0]
 
-                addInstructionsWithLabels(
-                    nextInstructionIndex,
-                    """
-                    invoke-static {v$buttonPressedRegister}, ${EXTENSION_CLASS_NAME}isButtonPressed(Ljava/lang/Object;)Z
-                    move-result v$freeRegister
-                    if-eqz v$freeRegister, :piko
-                    check-cast v$timelineItemRegister, $TIMELINE_ITEM_CLASS_NAME
-                    iget-object v$freeRegister, v$timelineItemRegister, $tweetFieldFromTimelineItem
-                    invoke-static {v$contextRegister,v$buttonPressedRegister,v$freeRegister}, ${EXTENSION_CLASS_NAME}buttonPressAction(Landroid/content/Context;Ljava/lang/Object;Ljava/lang/Object;)V
-                    return-void
-                    """.trimIndent(),
-                    ExternalLabel("piko", nextInstruction),
-                )
+                    val buttonEnumPressCheckIndex = indexOfFirstInstruction(contextIfEqzIndex, Opcode.IF_NE)
+                    val buttonPressedRegister = getInstruction(buttonEnumPressCheckIndex).registersUsed[0]
+
+                    val nextInstructionIndex = contextIfEqzIndex + 1
+                    val nextInstruction = getInstruction(nextInstructionIndex)
+                    val freeRegister = nextInstruction.registersUsed[0]
+
+                    addInstructionsWithLabels(
+                        nextInstructionIndex,
+                        """
+                        invoke-static {v$buttonPressedRegister}, ${EXTENSION_CLASS_NAME}isButtonPressed(Ljava/lang/Object;)Z
+                        move-result v$freeRegister
+                        if-eqz v$freeRegister, :piko
+                        move-object/from16 v$freeRegister, p$tweetObjectParameter
+                        invoke-static {v$contextRegister,v$buttonPressedRegister,v$freeRegister}, ${EXTENSION_CLASS_NAME}buttonPressAction(Landroid/content/Context;Ljava/lang/Object;Ljava/lang/Object;)V
+                        return-void
+                        """.trimIndent(),
+                        ExternalLabel("piko", nextInstruction),
+                    )
+                }
             }
         }
     }
