049e6c034429092b1b10b17f6fddba25a5a3dc69
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-07-11 20:56:41 +0300

    chore: Add resource mapping dependency, update gradle plugin and patches-library dependency

--- patches/src/main/kotlin/app/crimera/patches/instagram/misc/distractionFree/HideNotesTrayPatch.kt
+++ patches/src/main/kotlin/app/crimera/patches/instagram/misc/distractionFree/HideNotesTrayPatch.kt
@@ -13,11 +13,11 @@ import app.crimera.patches.instagram.utils.enableSettings
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import app.morphe.patcher.extensions.InstructionExtensions.getInstruction
-import app.morphe.patcher.extensions.InstructionExtensions.instructions
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.util.smali.ExternalLabel
 import app.morphe.patches.all.misc.resources.ResourceType
 import app.morphe.patches.all.misc.resources.resourceLiteral
+import app.morphe.patches.all.misc.resources.resourceMappingPatch
 import app.morphe.util.findFreeRegister
 import app.morphe.util.indexOfFirstInstruction
 import app.morphe.util.registersUsed
@@ -34,7 +34,7 @@ val hideNotesTrayPatch =
         name = "Hide notes tray",
         description = "Hides notes tray in DM section",
     ) {
-        dependsOn(settingsPatch)
+        dependsOn(settingsPatch, resourceMappingPatch)
         compatibleWith(COMPATIBILITY_INSTAGRAM)
 
         execute {
