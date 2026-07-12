d362f8d2d3a4d099cda571b6d895bdff739a1a6a
Utsavrajputt <mrutsav2024@gmail.com>
2026-07-12 23:33:47 +0530

    feat: Rebranded Piko as Modx

--- extensions/instagram/src/main/java/app/morphe/extension/instagram/settings/Settings.java
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/settings/Settings.java
@@ -68,8 +68,8 @@ public class Settings {
     public static final BooleanSetting DOWNLOAD_USERNAME_FOLDER = new BooleanSetting("download_username_folder", false);
     // Should be kept empty by default as its handled in `StorageUtils.java`
     public static final StringSetting CUSTOM_DOWNLOAD_PATH = new StringSetting("custom_download_path", "");
-    public static final BooleanSetting PIKO_SETTINGS_ON_ACTION_BAR = new BooleanSetting("piko_settings_on_action_bar", false);
-    public static final StringSetting PIKO_SETTINGS_ICON_STYLE = new StringSetting("piko_settings_icon_style", "x");
+    public static final BooleanSetting PIKO_SETTINGS_ON_ACTION_BAR = new BooleanSetting("piko_settings_on_action_bar", true);
+    public static final StringSetting PIKO_SETTINGS_ICON_STYLE = new StringSetting("piko_settings_icon_style", "gear");
     public static final BooleanSetting MONOCHROME_MORE_OPTIONS_ICONS = new BooleanSetting("monochrome_more_options_icons", false);
     public static final BooleanSetting PIKO_MORE_OPTIONS_USE_WIDGET_ICON = new BooleanSetting("piko_more_options_use_widget_icon", true);
     public static final StringSetting EXTERNAL_DOWNLOADER_PACKAGE_NAME = new StringSetting("external_downloader_package_name", "");
