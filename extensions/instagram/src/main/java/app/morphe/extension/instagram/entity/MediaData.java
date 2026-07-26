cdb905f63c3ef43bcdfc75291a82107df0f5b02b
Bikram <vicky.codemaster@gmail.com>
2026-07-25 18:38:15 -0700

    refactor(Instagram): Simplify video and image link retrieval by returning the first available variant

--- extensions/instagram/src/main/java/app/morphe/extension/instagram/entity/MediaData.java
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/entity/MediaData.java
@@ -252,38 +252,16 @@ public class MediaData extends Entity {
 
     public String getVideoLink() throws Exception {
         List<VideoData> videoDataList = this.getVideoVariants();
-        if(videoDataList != null && !videoDataList.isEmpty()){
-            VideoData highestResolutionVideo = videoDataList.get(0);
-            long highestPixelCount = (long) highestResolutionVideo.getWidth() * highestResolutionVideo.getHeight();
-
-            for (VideoData videoData : videoDataList) {
-                long pixelCount = (long) videoData.getWidth() * videoData.getHeight();
-                if (pixelCount > highestPixelCount) {
-                    highestResolutionVideo = videoData;
-                    highestPixelCount = pixelCount;
-                }
-            }
-
-            return highestResolutionVideo.getUrl();
+        if(videoDataList!=null){
+            return videoDataList.get(0).getUrl();
         }
         return null;
     }
     
     public String getImageLink() throws Exception {
         List<ImageData> imageDataList = this.getImageVariants();
-        if(imageDataList != null && !imageDataList.isEmpty()){
-            ImageData highestResolutionImage = imageDataList.get(0);
-            long highestPixelCount = (long) highestResolutionImage.getWidth() * highestResolutionImage.getHeight();
-
-            for (ImageData imageData : imageDataList) {
-                long pixelCount = (long) imageData.getWidth() * imageData.getHeight();
-                if (pixelCount > highestPixelCount) {
-                    highestResolutionImage = imageData;
-                    highestPixelCount = pixelCount;
-                }
-            }
-
-            return highestResolutionImage.getUrl();
+        if(imageDataList!=null){
+            return imageDataList.get(0).getUrl();
         }
         return null;
     }
