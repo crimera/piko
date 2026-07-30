350779d06bdf8c86992332387062bb42a6e236bc
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2026-07-31 00:19:28 +0900

    fix(Twitter): "Change app icon" is broken on recent X versions (#1624)

--- extensions/twitter/src/main/java/app/morphe/extension/twitter/patches/customise/appIcon/IconSelectorFragment.java
+++ extensions/twitter/src/main/java/app/morphe/extension/twitter/patches/customise/appIcon/IconSelectorFragment.java
@@ -14,30 +14,29 @@ import java.util.List;
 
 import app.morphe.extension.shared.ResourceType;
 import app.morphe.extension.shared.ResourceUtils;
-import app.morphe.extension.shared.Utils;
 
+@SuppressWarnings("deprecation")
 public class IconSelectorFragment extends Fragment {
 
     private AppIconManager iconManager;
-    private List<RowItem> rows;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        iconManager = new AppIconManager(getActivity());
+        iconManager = new AppIconManager();
 
         View root = inflater.inflate(ResourceUtils.getIdentifier(ResourceType.LAYOUT, "fragment_icon_selector"), container, false);
         ListView listView = root.findViewById(ResourceUtils.getIdentifier(ResourceType.ID, "icon_listview"));
 
         // Build all rows using the new builder class
         IconListBuilder builder = new IconListBuilder();
-        rows = builder.buildRows();
+        List<RowItem> rows = builder.buildRows();
 
         IconListAdapter adapter = new IconListAdapter(
                 getActivity(),
                 rows,
                 iconManager.getSavedIcon(),
-                componentName -> iconManager.applyIcon(componentName)
+                iconManager::applyIcon
         );
 
         listView.setAdapter(adapter);
