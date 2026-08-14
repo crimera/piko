3c05161c042022b96ccc8512cc56e6783723a982
Özer Öztaş <94235953+ozeroztas@users.noreply.github.com>
2026-08-14 05:31:03 +0300

    Fix reflective method lookup for 442 Media interfaces

--- extensions/instagram/src/main/java/app/morphe/extension/instagram/entity/Entity.java
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/entity/Entity.java
@@ -9,7 +9,6 @@ package app.morphe.extension.instagram.entity;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.lang.reflect.Constructor;
 
 public class Entity {
@@ -58,6 +57,33 @@ public class Entity {
         return new Entity(object);
     }
 
+    private Method findCompatibleMethod(Class<?> clazz, String methodName, Object... params) throws NoSuchMethodException {
+        for (Method method : clazz.getDeclaredMethods()) {
+            Class<?>[] types = method.getParameterTypes();
+            if (!method.getName().equals(methodName) || types.length != params.length) {
+                continue;
+            }
+
+            boolean compatible = true;
+            for (int i = 0; i < types.length; i++) {
+                Object param = params[i];
+                if (param == null) {
+                    continue;
+                }
+                if (!types[i].isAssignableFrom(param.getClass())) {
+                    compatible = false;
+                    break;
+                }
+            }
+
+            if (compatible) {
+                method.setAccessible(true);
+                return method;
+            }
+        }
+        throw new NoSuchMethodException(clazz.getName() + "." + methodName);
+    }
+
     public Object getMethod(Object clsObj, String methodName, Class<?>[] paramTypes, Object... params) throws Exception {
         Class<?> clazz;
         if (clsObj instanceof Class<?>) {
@@ -66,10 +92,14 @@ public class Entity {
             clazz = clsObj.getClass();
         }
 
-        Method method = clazz.getDeclaredMethod(methodName, paramTypes);
-        method.setAccessible(true);
-
-        return method.invoke(null, params);
+        try {
+            Method method = clazz.getDeclaredMethod(methodName, paramTypes);
+            method.setAccessible(true);
+            return method.invoke(clsObj instanceof Class<?> ? null : clsObj, params);
+        } catch (NoSuchMethodException e) {
+            Method method = findCompatibleMethod(clazz, methodName, params);
+            return method.invoke(clsObj instanceof Class<?> ? null : clsObj, params);
+        }
     }
 
     public Object getMethod(Object clsObj, String methodName, Object... params) throws Exception {
@@ -81,15 +111,16 @@ public class Entity {
         }
 
         if (params == null || params.length == 0) {
-            return clazz.getDeclaredMethod(methodName).invoke(clsObj);
-        } else {
-            Class<?>[] paramTypes = new Class<?>[params.length];
-            for (int i = 0; i < params.length; i++) {
-                paramTypes[i] = params[i].getClass();
-            }
-            return this.getMethod(clsObj, methodName, paramTypes, params);
+            Method method = clazz.getDeclaredMethod(methodName);
+            method.setAccessible(true);
+            return method.invoke(clsObj instanceof Class<?> ? null : clsObj);
         }
 
+        Class<?>[] paramTypes = new Class<?>[params.length];
+        for (int i = 0; i < params.length; i++) {
+            paramTypes[i] = params[i] == null ? Object.class : params[i].getClass();
+        }
+        return this.getMethod(clsObj, methodName, paramTypes, params);
     }
 
     public Object getMethod(String methodName, Class<?>[] paramTypes, Object... params) throws Exception {
