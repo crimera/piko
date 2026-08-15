3ca930e9d84a85f737c8f56e9e870b688f225ebb
Nao <280986462+naoverse06@users.noreply.github.com>
2026-08-15 18:56:16 +0900

    refactor(Instagram): rebind settings category icons (#1707)

--- extensions/instagram/src/main/java/app/morphe/extension/instagram/settings/preference/widgets/ButtonPref.java
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/settings/preference/widgets/ButtonPref.java
@@ -120,6 +120,7 @@ public class ButtonPref extends Preference {
     protected void onBindView(View view) {
         String key = getKey();
         InstagramPreferenceStyle.bindText(this, view);
+        InstagramPreferenceStyle.bindIcon(view, getIconResourceName(key));
         InstagramPreferenceStyle.setTrailingVisible(view, hasVisibleTrail(key));
         InstagramPreferenceStyle.setPressedHighlightEnabled(
                 view,
@@ -154,6 +155,9 @@ public class ButtonPref extends Preference {
     }
 
     private String getIconResourceName(String key) {
+        if (key == null) {
+            return null;
+        }
         if(key.equals(Constants.PIKO_FRAGMENT_ADS)){
             return UI.DRAWABLE_SHEILD_ICON;
         }
