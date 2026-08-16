7d3e29dc86b6aa59c8dd84400d2fe294f13f850f
inotia00 <108592928+inotia00@users.noreply.github.com>
2026-08-16 21:19:25 +0900

    fix(Twitter): Fix `Show sensitive media` patch

---
+++ extensions/twitter/stub/src/main/java/com/twitter/model/json/mediavisibility/JsonBlurredImageInterstitial.java
@@ -0,0 +1,8 @@
+package com.twitter.model.json.mediavisibility;
+
+public class JsonBlurredImageInterstitial {
+    // interstitialAction
+    public String d;
+    // verificationOptions
+    public Object e;
+}
