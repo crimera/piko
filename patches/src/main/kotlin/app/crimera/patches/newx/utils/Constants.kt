6eb5236af4088e19f1d3914f1ff9373f2c7d231d
crimera <44558091+crimera@users.noreply.github.com>
2026-09-09 19:39:21 +0800

    feat: support 12.24.0-prod.02

--- patches/src/main/kotlin/app/crimera/patches/newx/utils/Constants.kt
+++ patches/src/main/kotlin/app/crimera/patches/newx/utils/Constants.kt
@@ -18,6 +18,7 @@ internal object Constants {
                 AppTarget(version = "12.22.0-prod.01"),
                 AppTarget(version = "12.23.0-prod.01"),
                 AppTarget(version = "12.23.1-prod.01"),
+                AppTarget(version = "12.24.0-prod.02"),
                 AppTarget(version = "12.25.0-alpha.01", isExperimental = true),
             ),
         )
