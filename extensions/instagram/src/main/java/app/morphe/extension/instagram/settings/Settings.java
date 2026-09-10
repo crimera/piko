957b8f1a4915a40c38d50da33ab5e048603c3acc
votsz <207730596+ViictorNunes@users.noreply.github.com>
2026-09-10 17:59:50 -0300

    Add settings for profile and news navigation

--- extensions/instagram/src/main/java/app/morphe/extension/instagram/settings/Settings.java
+++ extensions/instagram/src/main/java/app/morphe/extension/instagram/settings/Settings.java
@@ -85,6 +85,9 @@ public class Settings {
     public static final BooleanSetting HIDE_NAVIGATION_DIRECT = new BooleanSetting("hide_navigation_direct", false);
     public static final BooleanSetting HIDE_NAVIGATION_SEARCH = new BooleanSetting("hide_navigation_search", false);
     public static final BooleanSetting HIDE_NAVIGATION_CREATE = new BooleanSetting("hide_navigation_create", false);
+    public static final BooleanSetting HIDE_NAVIGATION_PROFILE = new BooleanSetting("hide_navigation_profile", false);
+    public static final BooleanSetting HIDE_NAVIGATION_NEWS = new BooleanSetting("hide_navigation_news", false);
+    public static final StringSetting NAVIGATION_BUTTONS_ORDER = new StringSetting("navigation_buttons_order", "");
 
     public static final StringSetting ACTION_BAR_MAIN_FEED = new StringSetting("action_bar_main_feed", "");
     public static final StringSetting ACTION_BAR_USER_PROFILE = new StringSetting("action_bar_user_profile", "");
