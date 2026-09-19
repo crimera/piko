a82c8cc364737a4fd9edfa87da8be231a4ef59ac
Clo <273623891+chloricacid@users.noreply.github.com>
2026-09-19 22:07:58 +0300

    update(Twitter): Add HotYuriSex into unofficial instances for deeplinks

--- patches/src/main/kotlin/app/crimera/patches/twitter/link/customDeeplinks/CustomDeepLinksPatch.kt
+++ patches/src/main/kotlin/app/crimera/patches/twitter/link/customDeeplinks/CustomDeepLinksPatch.kt
@@ -58,6 +58,7 @@ val customDeepLinksPatch =
                             "mpregx.com",
                             "skibidix.com",
                             "girthyx.com",
+                            "hotyurisex.com",
                         ),
                     )
             }
